c9205cd4b7d17aa2921254de8155901671e7b154
ghlnh <lnh404gom@gmail.com>
2022-10-26 19:23:08 +0900

    20221026학습 내용

--- src/main/java/org/zerock/mapper/lecture/Mapper01.java
+++ src/main/java/org/zerock/mapper/lecture/Mapper01.java
@@ -1,8 +1,27 @@
 package org.zerock.mapper.lecture;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 import org.apache.ibatis.annotations.Select;
 
 public interface Mapper01 {
 	@Select("SELECT NOW()")
 	public String getCurrentTime();
+	
+	@Select("SELECT CustomerName FROM w3schools.Customers WHERE CustomerId =77")
+	public String getCustomerName();
+	
+	public String getEmployeeName();
+	
+	public String getProductName();
+	
+	public double getPrice();
+	
+	public int getQuantity();
+	
+	/* public Date getBirthDate(); */
+	//java.util.Date 나 java.sql.Date import해서 쓰기보다는 LocalDate 타입 이용해서 하는게 편리
+	public LocalDate getBirthDate();
+	
 }
