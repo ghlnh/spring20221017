a784c3683d9ec5adc529896c278b1a8eec01a31b
ghlnh <lnh404gom@gmail.com>
2022-10-27 17:46:07 +0900

    20221027 학습내용

---
+++ src/main/java/org/zerock/domain/lecture/JavaBean19.java
@@ -0,0 +1,16 @@
+package org.zerock.domain.lecture;
+
+import lombok.Data;
+
+@Data
+public class JavaBean19 {
+	private int id;
+	private String name;
+	private String conName;
+	private String address;
+	private String city;
+	private String postalCode;
+	private String country;
+	private String phone;
+	
+}
