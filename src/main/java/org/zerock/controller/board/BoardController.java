611be84d88781849463489783ddf7c03ecdbc069
ghlnh <lnh404gom@gmail.com>
2022-11-03 09:08:53 +0900

    20221101,1102 학습내용

--- src/main/java/org/zerock/controller/board/BoardController.java
+++ src/main/java/org/zerock/controller/board/BoardController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.board.BoardDto;
+import org.zerock.domain.board.PageInfo;
 import org.zerock.service.board.BoardSerivce;
 
 @Controller
@@ -32,10 +33,11 @@ public class BoardController {
 		
 		// business logic
 		int cnt = service.register(board);
-		if (cnt ==1) {
+		
+		if (cnt == 1) {
 			rttr.addFlashAttribute("message", "새 게시물이 등록되었습니다.");
-		}else {
-			rttr.addFlashAttribute("message", " 새 게시물이 등록되지 않았습니다.");
+		} else {
+			rttr.addFlashAttribute("message", "새 게시물이 등록되지 않았습니다.");
 		}
 		
 		// /board/list로 redirect
@@ -43,57 +45,92 @@ public class BoardController {
 	}
 	
 	@GetMapping("list")
-	public void list(Model model) {
+	public void list(
+			@RequestParam(name = "page", defaultValue = "1") int page,
+			@RequestParam(name = "t", defaultValue = "all") String type,
+			@RequestParam(name = "q", defaultValue = "") String keyword,
+			PageInfo pageInfo,
+			Model model) {
 		// request param
 		// business logic
-		List<BoardDto> list = service.listBoard();
+		List<BoardDto> list = service.listBoard(page, type, keyword, pageInfo);
 		
 		// add attribute
 		model.addAttribute("boardList", list);
 		// forward
 	}
+
+	// 위 list 메소드 파라미터 PageInfo에 일어나는 일을 풀어서 작성
+	/*
+	private void list2(
+			@RequestParam(name = "page", defaultValue = "1") int page,
+			HttpServletRequest request,
+			Model model) {
+		// request param
+		PageInfo pageInfo = new PageInfo();
+		pageInfo.setLastPageNumber(Integer.parseInt(request.getParameter("lastPageNumber")));
+		model.addAttribute("pageInfo", pageInfo);
+		
+		// business logic
+		List<BoardDto> list = service.listBoard(page, pageInfo);
+		
+		// add attribute
+		model.addAttribute("boardList", list);
+		// forward
+	}
+	*/
 	
-	@GetMapping("get")
-	public void get(int id, Model model) {
-		//request param
-		//business logic(게시물 db에서 가져오기)
+	
+	@GetMapping("get") 
+	public void get(
+			// @RequestParam 생략 가능
+			@RequestParam(name = "id") int id,
+			Model model) {
+		// req param
+		// business logic (게시물 db에서 가져오기)
 		BoardDto board = service.get(id);
-		System.out.println(board);
-		//add atribute
+		// add attribute
 		model.addAttribute("board", board);
-		//forward / redirect
+		// forward
+		
 	}
 	
 	@GetMapping("modify")
 	public void modify(int id, Model model) {
 		BoardDto board = service.get(id);
-		model.addAttribute("board",board);
+		model.addAttribute("board", board);
+		
 	}
 	
 	@PostMapping("modify")
-	public String modify(BoardDto board, RedirectAttributes rttr ) {
+	public String modify(BoardDto board, RedirectAttributes rttr) {
 		int cnt = service.update(board);
 		
-		if (cnt==1) {
-			rttr.addFlashAttribute("message2", board.getId() + "번 수정되었습니다.");
-		}else {
-			rttr.addFlashAttribute("message2", board.getId() + "번 수정되지 않았습니다.");
+		if (cnt == 1) {
+			rttr.addFlashAttribute("message", board.getId() + "번 게시물이 수정되었습니다.");
+		} else {
+			rttr.addFlashAttribute("message", board.getId() + "번 게시물이 수정되지 않았습니다.");
 		}
 		
 		return "redirect:/board/list";
 	}
 	
-	@PostMapping("remove") 
+	@PostMapping("remove")
 	public String remove(int id, RedirectAttributes rttr) {
 		int cnt = service.remove(id);
-		if (cnt==1) {
-			rttr.addFlashAttribute("message1", id +"번 게시물이 삭제되었습니다.");
-		}else {
-			rttr.addFlashAttribute("message1", id +"번 게시물이 삭제되지 않았습니다.");
+		
+		if (cnt == 1) {
+			// id번 게시물이 삭제되었습니다.
+			rttr.addFlashAttribute("message", id + "번 게시물이 삭제되었습니다.");
+		} else {
+			// id번 게시물이 삭제되지 않았습니다.
+			rttr.addFlashAttribute("message", id + "번 게시물이 삭제되지 않았습니다.");
 		}
 		
 		return "redirect:/board/list";
 	}
+	
+	
 }
 
 
--- src/main/webapp/WEB-INF/views/ex44/sub.jsp
+++ src/main/webapp/WEB-INF/views/ex44/sub.jsp
@@ -1,7 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ taglib prefix="my" tagdir="/WEB-INF/tags" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,44 +11,42 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-	<my:navBar active="register"></my:navBar>
-	<div class="container-md">
-		<div class="row">
-			<div class="col">
-				<h1>게시물 작성</h1>
-				<form action="" method="post">
-					<div class="mb-3">
-						<label for="" class="form-label">제목</label>
-						<input type="text" class="form-control" name="title">
-					</div>
-					<div class="mb-3">
-						<label for="" class="form-label">본문</label>
-						<textarea rows="5" class="form-control" name="content"></textarea>
-					</div>
-					<div class="mb-3">
-						<label for="" class="form-label">작성자</label>
-						<input type="text" class="form-control" name="writer">
-					</div>
-					
-					<input class="btn btn-primary" type="submit" value="등록">
-				
-				
-				</form>
-			</div>
-		</div>
-	</div>
-	
-	
+	<h1>json format data 전송</h1>
+	<button id="btn1">/ex44/sub01 post 요청 w/ data</button>
+	<br>
+	<button id="btn2">/ex44/sub02 post 요청 w/ data text/plain</button>
+	<br>
+	<button id="btn3">/ex44/sub02 post 요청 w/ data application/json</button>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script>
+const ctx = "${pageContext.request.contextPath}";
+
+document.querySelector("#btn3").addEventListener("click", function() {
+	fetch(ctx + "/ex44/sub03", {
+		method: "post",
+		headers : {
+			"Content-Type" : "application/json"
+		},
+		body : '{"name":"son", "address":"seoul"}'
+	});
+});
+
+document.querySelector("#btn2").addEventListener("click", function() {
+	fetch(ctx + "/ex44/sub02", {
+		method: "post",
+		body : '{"name":"son", "address":"seoul"}'
+	});
+});
+
+document.querySelector("#btn1").addEventListener("click", function() {
+	fetch(ctx + "/ex44/sub01", {
+		method: "post",
+		body : '{"name":"son", "address":"seoul"}'
+	});
+});
+</script>
 </body>
 </html>
-
-
-
-
-
-
-
 
 
 
