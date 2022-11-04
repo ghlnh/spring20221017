e9df1da95a3e09821e2ede9b2667a4e0bfde3378
ghlnh <lnh404gom@gmail.com>
2022-11-04 17:47:37 +0900

    20221104학습내용

--- src/main/java/org/zerock/controller/lecture/p09fetch/Controller45.java
+++ src/main/java/org/zerock/controller/lecture/p09fetch/Controller45.java
@@ -2,7 +2,9 @@ package org.zerock.controller.lecture.p09fetch;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -136,6 +138,48 @@ public class Controller45 {
 	@ResponseBody
 	public String method12() {
 		return "헬로 월드";
+	}
+	
+	@GetMapping("sub13")
+	@ResponseBody
+	public Map<String, String> method13() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("address", "서울");
+		map.put("email", "abc@gmail.com");
+		map.put("age", "33");
+		
+		return map;
+		
+	}
+	
+	@GetMapping("sub14")
+	@ResponseBody
+	public Map<String, Object> method14() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		JavaBean25 data = new JavaBean25();
+		JavaBean26 sub = new JavaBean26();
+		sub.setAddress(List.of("제주", " 울릉도"));
+		sub.setMarried(false);
+		data.setAge(99);
+		data.setInfo(sub);
+		map.put("address", "부산");
+		map.put("age", 33);
+		map.put("marreid", true);
+		map.put("data", data);
+		
+		return map;
+	}
+	
+	@GetMapping("sub15")
+	@ResponseBody
+	public Map<String, Object> method15() {
+	Map<String, Object> map = new HashMap<String, Object>();
+	
+	map.put("car", "tesla");
+	map.put("model", "avante");
+	map.put("color", List.of("blue","red"));
+	
+	return map;
 	}
 }
 
